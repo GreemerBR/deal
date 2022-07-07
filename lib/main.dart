9ed9d950a82774b30c0e3738101d812324b8a4c9
Gregory Viegas Zimmer <gregory.zimmer@warren.com.br>
2022-07-07 17:00:15 -0300

    [feat]
    Created myProfile

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,83 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        title: 'Deal', debugShowCheckedModeBanner: false, home: MyProfile()),
+  );
+}
+
+class MyProfile extends StatelessWidget {
+  const MyProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        toolbarHeight: 80,
+        backgroundColor: Color.fromARGB(255, 99, 66, 191),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: Text(
+            'Minha Conta',
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_rounded,
+            size: 35,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.edit_note_rounded,
+                size: 35,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 99, 66, 191),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 20),
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/Avatar.jpeg'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(300),
+                    ),
+                  ),
+                  Text('Gregory Viegas Zimmer'),
+                  Text('Blumenau - SC')
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+    ;
+  }
+}
