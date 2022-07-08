0756467a7646e59165aaf84cf003ae3b9bdfdd06
André-pm <andre.pereira.martins100@gmail.com>
2022-07-08 09:22:18 -0300

    [Feature] Add login screen

---
+++ lib/presenter/login/login.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/background_image_login.dart';
+import 'widgets/buttons_login.dart';
+import 'widgets/inputs_login.dart';
+import 'widgets/texts_login.dart';
+
+class Login extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              BackgroundImage(),
+              PeopleImage(),
+              DefaultTitle(),
+              DefaultInputWithoutIcon(),
+              DefaultInputWithIcon(),
+              DefaultButton(),
+              DefaultLinkText(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
