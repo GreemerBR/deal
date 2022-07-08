6599be8fb6d927a3e9fa9ffe7ca00f7b93bf913d
Gregory Viegas Zimmer <gregory.zimmer@warren.com.br>
2022-07-08 08:06:19 -0300

    [change]
    Changed the size of the text

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         centerTitle: true,
         toolbarHeight: 80,
         backgroundColor: Color.fromARGB(255, 99, 66, 191),
@@ -56,9 +57,9 @@ class MyProfile extends StatelessWidget {
               ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Container(
-                    margin: const EdgeInsets.only(left: 20),
                     width: 100,
                     height: 100,
                     decoration: BoxDecoration(
@@ -69,8 +70,29 @@ class MyProfile extends StatelessWidget {
                       borderRadius: BorderRadius.circular(300),
                     ),
                   ),
-                  Text('Gregory Viegas Zimmer'),
-                  Text('Blumenau - SC')
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Text(
+                      'Gregory Viegas Zimmer',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                      bottom: 50,
+                    ),
+                    child: Text(
+                      'Blumenau - SC',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                  )
                 ],
               ),
             )
