1fbea2d6e3c4cf26cafd23f09353874121169bad
Gregory Viegas Zimmer <gregory.zimmer@warren.com.br>
2022-07-11 16:26:29 -0300

    [change]
    Putting login folder in presenter folder

--- lib/presenter/login/login.dart
+++ lib/presenter/login/login.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/background_image_login.dart';
-import 'widgets/buttons_login.dart';
-import 'widgets/inputs_login.dart';
-import 'widgets/texts_login.dart';
+import 'widgets/background_image.dart';
+import 'widgets/default_button.dart';
+import 'widgets/default_input.dart';
+import 'widgets/default_link_text.dart';
+import 'widgets/default_title.dart';
+import 'widgets/people_image.dart';
 
 class Login extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -16,14 +18,13 @@ class Login extends StatelessWidget {
               children: [
                 BackgroundImage(),
                 PeopleImage(),
-                DefaultTitle(),
-                DefaultInputWithoutIcon(texto: 'Login'),
-                DefaultInputWithIcon(
-                    icon: Icon(Icons.remove_red_eye), texto: 'Senha'),
-                DefaultButton(),
-                DefaultLinkText(),
               ],
             ),
+                DefaultTitle(),
+                DefaultInput(texto: 'Login'),
+                DefaultInput(icon: Icon(Icons.remove_red_eye), texto: 'Senha'),
+                DefaultButton(),
+                DefaultLinkText(),
           ],
         ),
       ),
--- lib/presenter/login/widgets/background_image.dart
+++ lib/presenter/login/widgets/background_image.dart
@@ -10,29 +10,12 @@ class BackgroundImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
+      height: MediaQuery.of(context).size.height * 0.5,
       decoration: const BoxDecoration(
         image: DecorationImage(
             image: AssetImage('assets/images/background.png'),
             fit: BoxFit.fitWidth,
             alignment: Alignment(1, -1)),
-      ),
-    );
-  }
-}
-
-class PeopleImage extends StatelessWidget {
-  const PeopleImage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      top: 50,
-      child: Image.asset(
-        'assets/images/people.png',
-        width: 250,
       ),
     );
   }
