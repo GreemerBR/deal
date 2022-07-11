3a644534d5c2ac725de5c1ed9697c750302e4fe8
Henrique Da Silva Cardoso <Henrique.Cardoso@proway.treina>
2022-07-11 13:00:16 -0300

    [feat] changed fake search bar for real search bar

--- lib/presenter/splashScreen/splash_screen.dart
+++ lib/presenter/splashScreen/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     Future.delayed(
-      const Duration(seconds: 5),
+      const Duration(seconds: 4),
     ).then((value) {
       Navigator.of(context).push(
         MaterialPageRoute(
