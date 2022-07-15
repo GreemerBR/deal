5b58f9078cf9c723362278ab142c2e46fd261a4c
Henrique Da Silva Cardoso <Henrique.Cardoso@proway.treina>
2022-07-15 13:23:30 -0300

    [feat] made simulated navigation

--- lib/presenter/main_menu/widgets/card_product_ad.dart
+++ lib/presenter/main_menu/widgets/card_product_ad.dart
@@ -1,3 +1,4 @@
+import 'package:app_2/presenter/my_profile/my_profile_page.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
@@ -29,97 +30,111 @@ class _CardProductAdState extends State<CardProductAd> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      width: double.maxFinite,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Color(0xffa9a9a9),
-            spreadRadius: 0,
-            blurRadius: 5,
-            offset: Offset(0, 5),
+    return MaterialButton(
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return MyProfilePage();
+            },
           ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 125,
-                width: 190,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      widget.imageLink,
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xffa9a9a9),
+              spreadRadius: 0,
+              blurRadius: 5,
+              offset: Offset(0, 5),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 125,
+                  width: 190,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        widget.imageLink,
+                      ),
+                      fit: BoxFit.fitHeight,
                     ),
-                    fit: BoxFit.fitHeight,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
-                  ),
-                  border: Border.all(
-                    width: 0.5,
-                    color: Color.fromARGB(255, 192, 180, 225),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                    ),
+                    border: Border.all(
+                      width: 0.5,
+                      color: Color.fromARGB(255, 192, 180, 225),
+                    ),
                   ),
                 ),
-              ),
-              IconButton(
-                onPressed: () {
-                  makeFavorite();
-                },
-                icon: Icon(
-                  widget.isFavorite ? Icons.star :  Icons.star_border,
-                  color: (
-                    widget.isFavorite
-                      ? Color.fromARGB(
-                          255,
-                          99,
-                          66,
-                          191,
-                        )
-                      : Colors.grey),
+                IconButton(
+                  onPressed: () {
+                    makeFavorite();
+                  },
+                  icon: Icon(
+                    widget.isFavorite ? Icons.star :  Icons.star_border,
+                    color: (
+                      widget.isFavorite
+                        ? Color.fromARGB(
+                            255,
+                            99,
+                            66,
+                            191,
+                          )
+                        : Colors.grey),
+                  ),
+                  splashRadius: 1,
                 ),
-                splashRadius: 1,
-              ),
-            ],
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.productName,
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14,
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.productName,
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Text(
-                    'R\$ ' + widget.productPrice,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text(
-                    widget.productLocation,
-                    style: TextStyle(
-                      fontSize: 10,
-                      color: Colors.grey,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: Text(
+                        'R\$ ' + widget.productPrice,
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ),
-                  )
-                ],
+                    Text(
+                      widget.productLocation,
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
