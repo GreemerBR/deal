f333c63d677d8002d4f840c91db8feef31270196
Gregory Viegas Zimmer <Gregory.Zimmer@proway.treina>
2022-07-18 15:18:09 -0300

    [fix] Fixed import

--- lib/presenter/main_menu/widgets/card_product_ad.dart
+++ lib/presenter/main_menu/widgets/card_product_ad.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../my_profile/my_profile_page.dart';
+import '../../announcement/announce_page.dart';
 
 // ignore: must_be_immutable
 class CardProductAd extends StatefulWidget {
