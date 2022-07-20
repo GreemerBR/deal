27517a03dc0f7b3a59de7db82005a10d22a70949
André Pereira Martins <Andr.Martins@proway.treina>
2022-07-20 15:21:19 -0300

    [fix] changing branch

--- lib/presenter/second_splash/splash_screen_page.dart
+++ lib/presenter/second_splash/splash_screen_page.dart
@@ -1,16 +1,10 @@
-// ignore_for_file: unused_import
-
+import 'package:app_2/is_logged/is_logged_page.dart';
 import 'package:flutter/material.dart';
 
 import '../../core/app_assets.dart';
-<<<<<<< HEAD
 import '../../is_logged/is_logged_page.dart';
-import '../login/login_page.dart';
-=======
->>>>>>> e35115720dc92acb2f7e65f91e1268e92ec701bc
 import '../login/widgets/default_title.dart';
 import 'widgets/animated_slide_up.dart';
-import 'widgets/gesture.dart';
 
 class SecondSplashScreen extends StatefulWidget {
   const SecondSplashScreen({Key? key}) : super(key: key);
