b6dad2e28bbda6ee0728fc8e595c0d5d83d2f6e0
André Pereira Martins <andre.pereira.martins100@gmail.com>
2022-08-04 22:27:08 -0300

    [feature] adding animation in login and announces screen

--- lib/presenter/login/login_page.dart
+++ lib/presenter/login/login_page.dart
@@ -1,5 +1,7 @@
+import 'package:app_2/core/app_assets.dart';
 import 'package:app_2/core/get_it.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 import '../../core/database.dart';
 import 'widgets/circle_avatar.dart';
@@ -24,13 +26,22 @@ class LoginPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              SizedBox(height: 35),
               DefaultTitle(
                 title: 'Já possui conta?',
                 subtitle: 'Entre agora mesmo',
                 colortitle: Colors.white,
                 colorSubtitle: Colors.white,
               ),
-              ProfileAvatar(),
+              // ProfileAvatar(),
+              SizedBox(height: 10),
+              Lottie.asset(
+                loginAnimation,
+                repeat: false,
+                width: 120,
+                height: 120,
+                fit: BoxFit.fill,
+              ),
               LoginWidget(),
               GoogleButton(),
               DefaultLinkText(),
